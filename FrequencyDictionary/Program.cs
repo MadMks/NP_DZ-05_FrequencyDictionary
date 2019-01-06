5a1ce3b4210a39317b634ab8f6856fc8422ce113
Makc <mks59k@gmail.com>
2019-01-06 19:04:57 +0200

    Создание формы.

--- FrequencyDictionary/Program.cs
+++ FrequencyDictionary/Program.cs
@@ -16,7 +16,7 @@ namespace FrequencyDictionary
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
