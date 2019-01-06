66e1c8c1eeb908ecfff0d23bf8c00fa661903b31
Makc <mks59k@gmail.com>
2019-01-06 19:32:15 +0200

    Визуальные настройки приложения.

--- FrequencyDictionary/MainForm.Designer.cs
+++ FrequencyDictionary/MainForm.Designer.cs
@@ -28,20 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelUrl = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.buttonParse = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // labelUrl
+            // 
+            this.labelUrl.AutoSize = true;
+            this.labelUrl.Location = new System.Drawing.Point(13, 23);
+            this.labelUrl.Name = "labelUrl";
+            this.labelUrl.Size = new System.Drawing.Size(24, 13);
+            this.labelUrl.TabIndex = 0;
+            this.labelUrl.Text = "Url:";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(43, 20);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(404, 21);
+            this.textBoxUrl.TabIndex = 1;
+            // 
+            // buttonParse
+            // 
+            this.buttonParse.Location = new System.Drawing.Point(453, 19);
+            this.buttonParse.Name = "buttonParse";
+            this.buttonParse.Size = new System.Drawing.Size(75, 23);
+            this.buttonParse.TabIndex = 2;
+            this.buttonParse.Text = "Parse";
+            this.buttonParse.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 60);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(516, 281);
+            this.textBox2.TabIndex = 3;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 409);
+            this.ClientSize = new System.Drawing.Size(544, 353);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonParse);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.labelUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Частотный словарь";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelUrl;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.Button buttonParse;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
